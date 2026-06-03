ba13be9014264b8d5ec0a6d4624590dcee8eda3e
vishnureddi2306 <101704840+vishnureddi2306@users.noreply.github.com>
2026-06-03 09:25:34 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    newrelic = {
+      source  = "newrelic/newrelic"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "newrelic" {
+  account_id = var.newrelic_account_id
+  api_key    = var.newrelic_api_key
+  region     = var.newrelic_region
+}
